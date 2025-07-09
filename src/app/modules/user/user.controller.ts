f2678d630d4f6f9c8e9bd6a9f5945f2c734f1099
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-09 16:55:00 +0600

    global error handle

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,9 +2,11 @@
 import httpStatus from "http-status";
 import { NextFunction, Request, Response } from "express";
 import { userServices } from "./user.service";
+import { AppError } from "../../errors/AppError";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        throw new AppError(httpStatus.BAD_REQUEST, "fake")
         const user = await userServices.userCreateService(req.body);
         res.status(httpStatus.CREATED).json({
             success: true,
