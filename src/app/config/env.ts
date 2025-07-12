d4ffa24343d232c27364b80fef3f9eb0f35c6e42
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-12 16:06:42 +0600

    jwt utils created

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -5,11 +5,12 @@ interface IEnv {
     PORT: string;
     DB_URL: string;
     NODE_ENV: "development" | "production";
-    JWT_SECRET: string
+    JWT_SECRET: string,
+    JWT_EXPIRES_IN: string
 };
 
 const loadEnvVariable = (): IEnv => {
-    const requiredVariable: string[] = ["PORT", "MONGO_URI", "NODE_ENV", "JWT_SECRET"];
+    const requiredVariable: string[] = ["PORT", "MONGO_URI", "NODE_ENV", "JWT_SECRET", "JWT_EXPIRES_IN"];
     requiredVariable.forEach((key) => {
         if (!process.env[key]) {
             throw new Error(`Missing required environment variable: ${key}`);
@@ -19,7 +20,8 @@ const loadEnvVariable = (): IEnv => {
         PORT: process.env.PORT as string,
         DB_URL: process.env.MONGO_URI as string,
         NODE_ENV: process.env.NODE_ENV as "development" | "production",
-        JWT_SECRET: process.env.JWT_SECRET as string
+        JWT_SECRET: process.env.JWT_SECRET as string,
+        JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string
     };
 };
 
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,8 +3,8 @@ import httpStatus from 'http-status';
 import { AppError } from "../../errors/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
-import jwt from "jsonwebtoken";
 import { envVars } from "../../config/env";
+import { generateToken } from "../../utils/jwt";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
@@ -25,7 +25,7 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         email: isExist.email,
         role: isExist.role
     };
-    const accessToken = jwt.sign(jwtPayload, envVars.JWT_SECRET, { expiresIn: "1d" });
+    const accessToken = generateToken(jwtPayload, envVars.JWT_SECRET, envVars.JWT_EXPIRES_IN);
 
     return {
         accessToken
