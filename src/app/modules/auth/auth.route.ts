7a08a1167222eef27c31e193080430ed7e1dbb76
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-14 10:09:01 +0600

    implement reset password

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
 
 const router = Router();
 
 router.post("/login", authController.credentialsLogin);
 router.post("/refresh-token", authController.refreshTokenLogin);
 router.post("/logout", authController.logout);
+router.post("/reset-password", checkAuth("ADMIN", "SUPER_ADMIN", "USER", "GUIDE"), authController.resetPassword);
 
 export const authRouter = router;
