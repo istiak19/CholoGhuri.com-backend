f5f65a6be06e8fd76467feab2a6d9cfc44a0e055
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-17 11:32:09 +0600

    handle error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,25 +1,61 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 import { envVars } from "../config/env";
 import { AppError } from "../errors/AppError";
+import { Request, Response, NextFunction } from "express";
+import { handleZodError } from "../errors/handleZodError";
+import { handleCastError } from "../errors/handleCastError";
+import { handleDuplicateError } from "../errors/handleDuplicateError";
+import { handleMongooseValidationError } from "../errors/handleMongooseValidationError";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "Something went wrong";
+
+    //  // Duplicate Key Error
     if (err.code === 11000) {
-        statusCode = 400;
-        const duplicatedEmail = Object.keys(err.keyValue)[0];
-        message = `${err.keyValue[duplicatedEmail]} already exists.`;
-    } else if (err.name === "CastError") {
-        statusCode = 400;
-        message = "Invalid MongoDB ObjectId. Please provide a valid ID"
-    } else if (err instanceof AppError) {
-        statusCode = err.statusCode
-        message = err.message
-    } else if (err instanceof Error) {
-        message = err.message
+        const simplified = handleDuplicateError(err);
+        statusCode = simplified.statusCode;
+        message = simplified.message;
     }
+
+    // CastError
+    else if (err.name === "CastError") {
+        const simplified = handleCastError(err);
+        statusCode = simplified.statusCode;
+        message = simplified.message;
+    }
+
+    // Mongoose ValidationError
+    else if (err.name === "ValidationError") {
+        const simplified = handleMongooseValidationError(err);
+        return res.status(simplified.statusCode).json({
+            success: false,
+            message: simplified.message,
+            errorMessages: simplified.errorMessages,
+        });
+    }
+
+    // Handle Zod Validation Error
+    else if (err instanceof ZodError) {
+        const simplified = handleZodError(err);
+        return res.status(simplified.statusCode).json({
+            success: false,
+            message: simplified.message,
+            errorMessages: simplified.errorMessages,
+        });
+    }
+
+    //   // AppError
+    else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+    } else if (err instanceof Error) {
+        message = err.message;
+    }
+
+    // Default error response
     res.status(statusCode).json({
         success: false,
         message,
