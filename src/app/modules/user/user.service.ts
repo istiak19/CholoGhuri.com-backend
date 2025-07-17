8e4fcdd0fd7c40aac1496beec8339c473a6419aa
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-17 10:34:25 +0600

    Mongoose Validation Error

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -16,12 +16,15 @@ const userAllGetService = async () => {
 
 const userCreateService = async (payload: Partial<IUser>) => {
     const { email, password, ...rest } = payload;
-    const isExist = await User.findOne({ email });
-    if (isExist) {
-        throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
-    };
+    // const isExist = await User.findOne({ email });
+    // if (isExist) {
+    //     throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
+    // };
     const hashPassword = await bcrypt.hash(password as string, 10);
-    const auth: IAuthProvider = { provider: "credentials", providerId: email as string }
+    const auth: IAuthProvider = {
+        provider: "credentials",
+        providerId: email as string
+    };
     const user = await User.create({
         email,
         password: hashPassword,
