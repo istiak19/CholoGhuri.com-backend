2aa8e510a8563c369d022e5dd9d1ea6c6fe234d9
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-23 20:48:35 +0600

    single get route created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -7,7 +7,9 @@ import { createUserZodSchema, updatedUserZodSchema } from "./user.validation";
 const router = Router();
 
 router.get("/all-user", checkAuth("ADMIN", "SUPER_ADMIN"), userControllers.allGetUser);
+router.get("/get-me", checkAuth("ADMIN", "SUPER_ADMIN", "USER", "GUIDE"), userControllers.GetUserMe);
 router.post("/register", validateRequest(createUserZodSchema), userControllers.createUser);
+router.get("/:id", checkAuth("ADMIN", "SUPER_ADMIN", "USER", "GUIDE"), userControllers.userSingleGet);
 router.patch("/:id", validateRequest(updatedUserZodSchema), checkAuth("ADMIN", "SUPER_ADMIN", "USER", "GUIDE"), userControllers.UpdateUser);
 
 export const userRouters = router;
